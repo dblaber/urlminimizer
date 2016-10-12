c5ee4734502f139c03553e59eb7f1fb3758af6e5
dmb <dmb@thinkpad>
2016-10-11 22:20:46 -0400

    More initial

--- UrlMinifierLibrary/src/org/da4/urlminimizer/XmlConfiguration.java
+++ UrlMinifierLibrary/src/org/da4/urlminimizer/XmlConfiguration.java
@@ -1,5 +1,25 @@
 package org.da4.urlminimizer;
 
-public class XmlConfiguration {
+import java.io.File;
+import java.io.IOException;
 
+import org.da4.urlminimizer.exception.ConfigException;
+import org.da4.urlminimizer.vo.ConfigVO;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+public class XmlConfiguration {
+	public ConfigVO getConfig(String filename) throws ConfigException
+	{
+		 SAXBuilder builder = new SAXBuilder();
+		 try {
+			builder.build(new File(filename));
+		} catch (JDOMException e) {
+			throw new ConfigException("Error Parsing Config File",e);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new ConfigException("Error Reading Config File",e);
+		}
+		 
+	}
 }
--- UrlMinifierLibrary/src/org/da4/urlminimizer/vo/ConfigVO.java
+++ UrlMinifierLibrary/src/org/da4/urlminimizer/vo/ConfigVO.java
@@ -1,6 +1,9 @@
 package org.da4.urlminimizer.vo;
 
+import java.util.List;
+
 public class ConfigVO {
-	
+	String productName;
+	List<PluginVO> pluginConfigs;
 
 }
