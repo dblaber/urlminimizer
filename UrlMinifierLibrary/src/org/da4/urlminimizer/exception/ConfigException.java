d0c5d276e54ef4b5ed6709b3730dfc4a4dcceaaf
Darren Blaber <dmbtech@gmail.com>
2016-10-11 22:20:46 -0400

    More initial

---
+++ UrlMinifierLibrary/src/org/da4/urlminimizer/exception/ConfigException.java
@@ -0,0 +1,10 @@
+package org.da4.urlminimizer.exception;
+
+public class ConfigException extends Exception {
+	public ConfigException(String msg, Throwable e) {
+		super(msg,e);
+	}
+	public ConfigException(String msg) {
+		super(msg);
+	}
+}
