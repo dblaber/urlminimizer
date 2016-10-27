0cb64a27ccf5a946f233e72583c481a9d626ee2b
Darren Blaber <dmbtech@gmail.com>
2016-10-27 00:02:44 -0400

    Update DDL

--- DDL/UrlMinifierV1.sql
+++ DDL/UrlMinifierV1.sql
@@ -23,3 +23,38 @@ CREATE UNIQUE INDEX "alias_pk"
 CREATE UNIQUE INDEX "destination_url_idx"
 	ON "minifier"."minimized_urls" USING btree ("destination_url");
 
+CREATE TABLE minifier.stats_clicks  ( 
+    minified_alias 	varchar(100) NOT NULL,
+    click_cnt      	int8 NULL,
+    last_clicked_ts	timestamp NULL,
+    PRIMARY KEY(minified_alias)
+)
+WITHOUT OIDS 
+TABLESPACE pg_default
+GO
+ALTER TABLE minifier.stats_clicks
+    ADD CONSTRAINT stat_clicks_alias_fk
+	FOREIGN KEY(minified_alias)
+	REFERENCES minifier.minimized_urls(minified_alias)
+	ON DELETE NO ACTION 
+	ON UPDATE NO ACTION 
+
+CREATE TABLE minifier.stats_click_log  ( 
+    click_ts      	timestamp NOT NULL,
+    user_agent    	varchar NULL,
+    minified_alias	varchar(100) NOT NULL,
+    referrer      	varchar NULL,
+    source_ip     	varchar(100) NULL 
+    )
+WITHOUT OIDS 
+TABLESPACE pg_default
+GO
+ALTER TABLE minifier.stats_click_log
+    ADD CONSTRAINT stats_log_fk
+	FOREIGN KEY(minified_alias)
+	REFERENCES minifier.minimized_urls(minified_alias)
+	ON DELETE NO ACTION 
+	ON UPDATE NO ACTION 
+CREATE INDEX stats_clicks_log_idx
+    ON minifier.stats_click_log USING btree (minified_alias text_ops, click_ts timestamp_ops)
+GO
